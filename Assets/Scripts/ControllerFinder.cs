5996b08edc0fa90da2bbab3ce367135277eb469a
19craftman <47764907+19craftman@users.noreply.github.com>
2020-11-21 19:58:17 -0500

    player selection
    combining what i got

---
+++ Assets/Scripts/ControllerFinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class ControllerFinder : MonoBehaviour
+{
+    public static event System.Action<ControllerStatusChangedEventArgs> ControllerConnectedEvent;
+    public Controller[] activecontrollers;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+   
+}
